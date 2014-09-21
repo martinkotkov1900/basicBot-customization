5901ca7c99893a1024915bc4c6043dc4ec29ac0c
martinkotkov1900 <martintkokv@ejoijfw.com>
2014-09-21 15:21:09 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -34,7 +34,7 @@
 
         bot.commands.baconCommand = {
             command: 'bacon',  //The command to be called. With the standard command literal this would be: !bacon
-            rank: 'bouncer', //Minimum user permission to use the command
+            rank: 'user', //Minimum user permission to use the command
             type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
             functionality: function (chat, cmd) {
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
